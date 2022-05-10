8186f34cb1fa964bcf8fe425d24b8ec620cd54af
Jonathan <jonathankwon@yahoo.com>
2022-05-09 19:00:14 -0700

    Functionality to addToList, handleCheck, handleDelete, css styling

--- todo-list/src/components/todoList.jsx
+++ todo-list/src/components/todoList.jsx
@@ -1,45 +1,82 @@
 import React, {useState} from 'react'
 
 //initial todo list
-const myTodoList = [
-    'Pass the belt exam',
-    'Finish all core assignments',
-    'Submit a MVP project',
-    'Catch up on One Piece',
-    'Find a studio apartment'
-];
+// const myTodoList = [
+//     'Pass the belt exam',
+//     'Finish all core assignments',
+//     'Submit a MVP project',
+//     'Catch up on One Piece',
+//     'Find a studio apartment'
+// ];
 
 export default function TodoList() {
+    const [list, setList] = useState([
+        // {item:"Catch up on One Piece", checked: false},{item:"Watched latest episode of Spy x Family", checked:false},{item:"Finish Todo assignment", checked: false}
+    ])
     const [inputVal, setInputVal] = useState('');
-    const [checkItem, setCheckItem] = useState(false);
+    // const [inputError, setInputError] = useState('');
 
-    function handleSubmit(e) {
+    function addToList(e) {
         e.preventDefault();
-        console.log('click')
+        // if (e.length < 1) {
+        //     setInputError('Todo list item cannot be blank!')
+        // } else if (e.length < 5) {
+        //     setInputError('Must be longer than 5 characters!')
+        // }
+        const updateList = [...list];
+        updateList.push({item: inputVal, checked: false});
+        setList(updateList);
+        setInputVal('');
+        // console.log(updateList)
     }
 
-    function handleDelete(e) {
-        e.preventDefault();
-        console.log('clicked')
+    function handleCheck (e) {
+        const checkBox = list.map((item, id) => {
+            if (id === e) {
+                const updatedList = {...item,checked: !item.checked}
+
+                return updatedList
+            }
+            
+            return item
+        })
+
+        setList(checkBox);
+
+        
+        // console.log(e.target.checked);
+    }
+
+    function handleDelete(i) {
+        i.preventDefault();
+        const deleteListItem = list.map((item, id) => {
+                return id !== i;
+        })
+        setList(deleteListItem);
+        // console.log('clicked')
     }
 
     return (
-        <div>
-            <h1>My Todo List</h1>
-            <form onSubmit={handleSubmit}>
+        <div style={{margin:'40px', textAlign:'center', fontFamily:'sans-serif'}}>
+            <h1 style={{color:'orange'}}>My Todo List</h1>
+            <form onSubmit={addToList}>
+                <p>Type in what you want to add to your todo list below.</p>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
+                {/* {inputError && <p style={{color:'red'}}>{inputError}</p>} */}
                 <button>Add to list</button>
             </form>
-            {inputVal}
-            {myTodoList.map((value, i) => 
-                <div style={{display:'flex', justifyContent:'space-evenly'}}>
-                    <p key={i}>{value}</p>
-                    <label key={i}>
-                        <input type="checkbox" checked={checkItem} onChange={e => setCheckItem(e.target.checked)}/>
-                    </label>
-                    <input key={i} type="submit" value='Delete' onClick={handleDelete}/>
-                </div>
+
+            {list.map((value, i) => 
+
+                <form key={i} style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+                    <div style={{display:'flex', alignItems:'center'}}>
+                        <p className={value.checked ? 'line-through' : ''}>{value.item}</p>
+                        <input type="checkbox" checked={value.checked} onChange={(e) => handleCheck(i)}/>
+                    </div>
+                    <input value="Delete" type="submit" onClick={handleDelete}/>
+                </form>
             )}
+            {/* {JSON.stringify(list)} */}
         </div>
 
     );
