428a561f6e3a673454a34384f19329cdbe5147e4
Jonathan <jonathankwon@yahoo.com>
2022-05-10 17:54:44 -0700

    fixed delete function, added validation

--- todo-list/src/components/todoList.jsx
+++ todo-list/src/components/todoList.jsx
@@ -4,7 +4,7 @@ import React, {useState} from 'react'
 // const myTodoList = [
 //     'Pass the belt exam',
 //     'Finish all core assignments',
-//     'Submit a MVP project',
+//     'Submit a MVP project',         // didn't use this array
 //     'Catch up on One Piece',
 //     'Find a studio apartment'
 // ];
@@ -14,15 +14,18 @@ export default function TodoList() {
         // {item:"Catch up on One Piece", checked: false},{item:"Watched latest episode of Spy x Family", checked:false},{item:"Finish Todo assignment", checked: false}
     ])
     const [inputVal, setInputVal] = useState('');
-    // const [inputError, setInputError] = useState('');
+    const [inputError, setInputError] = useState('');
 
+    // function to add a string to todolist
     function addToList(e) {
         e.preventDefault();
-        // if (e.length < 1) {
-        //     setInputError('Todo list item cannot be blank!')
-        // } else if (e.length < 5) {
-        //     setInputError('Must be longer than 5 characters!')
-        // }
+        console.log(e);
+        if (inputVal.length < 1) {
+            return setInputError('Todo list item cannot be blank!')
+        } else if ( inputVal.length < 5) {
+            return setInputError('Must be longer than 5 characters!')
+        }
+        setInputError('')
         const updateList = [...list];
         updateList.push({item: inputVal, checked: false});
         setList(updateList);
@@ -33,23 +36,19 @@ export default function TodoList() {
     function handleCheck (e) {
         const checkBox = list.map((item, id) => {
             if (id === e) {
-                const updatedList = {...item,checked: !item.checked}
-
+                const updatedList = {...item,checked: !item.checked} // Change the boolean value to opposite
                 return updatedList
             }
-            
             return item
         })
-
         setList(checkBox);
-
-        
         // console.log(e.target.checked);
     }
 
+    // function to delete list item grabbed by the index
     function handleDelete(i) {
-        i.preventDefault();
-        const deleteListItem = list.map((item, id) => {
+        console.log(i);
+        const deleteListItem = list.filter(( id) => {
                 return id !== i;
         })
         setList(deleteListItem);
@@ -59,10 +58,10 @@ export default function TodoList() {
     return (
         <div style={{margin:'40px', textAlign:'center', fontFamily:'sans-serif'}}>
             <h1 style={{color:'orange'}}>My Todo List</h1>
-            <form onSubmit={addToList}>
+            <form onSubmit={addToList}> 
                 <p>Type in what you want to add to your todo list below.</p>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
-                {/* {inputError && <p style={{color:'red'}}>{inputError}</p>} */}
+                {inputError && <p style={{color:'red'}}>{inputError}</p>}
                 <button>Add to list</button>
             </form>
 
@@ -73,10 +72,10 @@ export default function TodoList() {
                         <p className={value.checked ? 'line-through' : ''}>{value.item}</p>
                         <input type="checkbox" checked={value.checked} onChange={(e) => handleCheck(i)}/>
                     </div>
-                    <input value="Delete" type="submit" onClick={handleDelete}/>
+                    <button onClick={() => handleDelete(i)}>Delete</button>
                 </form>
             )}
-            {/* {JSON.stringify(list)} */}
+            {JSON.stringify(list)}
         </div>
 
     );
